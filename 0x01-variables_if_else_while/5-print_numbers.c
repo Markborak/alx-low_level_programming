52a59eea8f3cb7d56e3dffadd188d7246211052c
Markborak <markborak4@gmail.com>
2023-06-17 04:45:13 -0700

    variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Programe that prints starting from zero and less than ten
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
