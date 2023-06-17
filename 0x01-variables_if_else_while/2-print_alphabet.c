55ff791fc589af4d4465fcc48da95c919325b15a
Markborak <markborak4@gmail.com>
2023-06-17 03:24:08 -0700

    variables_putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Serves as entry point of the programe which prints A to Z
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int x;
+for (x = 'A'; x <= 'Z'; x++)
+{
+putchar(x);
+putchar('\n');
+}
+return (0);
+}
