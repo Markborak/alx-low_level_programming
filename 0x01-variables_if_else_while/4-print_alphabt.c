cb4206867c2fd20415157b7b49c0082af697e69c
Markborak <markborak4@gmail.com>
2023-06-17 04:18:48 -0700

    variables_putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Programe prints all lowercase letters except q and e
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 'a'; i <= 'z' && i != 'q'; i++)
+{
+if (i != 'e' && 'i' != 'q')
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
