c6a1f29b3ed199b0de90ebb8a4903ce56d8b77b9
Markborak <markborak4@gmail.com>
2023-06-18 12:05:29 -0700

    variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if ((i != 98 || j != 99) && i % 10 <= j % 10)
+if ((i != 98 && j != 99) && i % 10 <= j % 10)
 
 {
 putchar(',');
