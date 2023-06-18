db534ac7bfe25af7971c52d794641860895132e1
Markborak <markborak4@gmail.com>
2023-06-18 11:15:21 -0700

    variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Program that prints double digits in twos
  *
@@ -6,26 +7,25 @@
  */
 int main(void)
 {
-int i;
-int j;
-for (i = 0; i <= 99; i++)
-{
-for (j = i + 1; j <= 99; j++)
-{
-if (i != 98 || j != 99)
-{
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
-putchar(' ');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+  int i;
+  int j;
+
+  for (i = 0; i <= 99; i++)
+  {
+    for (j = i + 1; j <= 99; j++)
+    {
+      if (i % 10 <= j % 10)
+      {
+        putchar(i / 10 + '0');
+        putchar(i % 10 + '0');
+        putchar(' ');
+        putchar(j / 10 + '0');
+        putchar(j % 10 + '0');
+        putchar(',');
+        putchar(' ');
+      }
+    }
+  }
+  putchar('\n');
+  return (0);
 }
