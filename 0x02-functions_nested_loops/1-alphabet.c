9061839c10651611467e7cb04c4938795b3954fc
Markborak <markborak4@gmail.com>
2023-06-20 11:30:53 +0300

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Function that prints the alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+void print_alphabet(void);
+for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
+{
+_putchar(a);
+}
+_putchar('\n')
+return (0);
+}
