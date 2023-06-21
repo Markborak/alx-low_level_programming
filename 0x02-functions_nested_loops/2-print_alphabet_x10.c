83b838cc419d826dbee571ddf4613a47989de807
Markborak <markborak4@gmail.com>
2023-06-21 07:18:56 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * add - Adds two integers.
- * @i: The first integer.
- * @j: The second integer.
- *
- * Return: The sum of the two integers.
+ * print_alphabet_x10 - Prints 10 times the alphabet,
+ * in lowercase followed by a new line
  */
 
-int add(int i, int j)
+void print_alphabet_x10(void)
 {
-	return (i + j);
+	int count = 0;
+	char lower_case;
+
+	while (count++ <= 9)
+	{
+		for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+			_putchar(lower_case);
+		_putchar('\n');
+	}
 }
