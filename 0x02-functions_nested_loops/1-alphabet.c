503855fd00bdef6e21badcbeb48a3093cdb4cfc4
Markborak <markborak4@gmail.com>
2023-06-21 01:38:54 -0700

    :loops:

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 /**
-* print_alphabet - print all alphabet in lowercase
+* main - Print alphabet
+*
+* Return: Always 0 (success)
 */
-void print_alphabet(void)
+int main(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
+{
 putchar(letter);
+}
 putchar('\n');
+return (0);
 }
