341727cfd51867bc4148a2da0e9b3a1378d001f4
Markborak <markborak4@gmail.com>
2023-06-22 07:34:28 -0700

    debuging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
+/**
+ * largest_number - finds the largest number among three integers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: the largest number
+ */
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
