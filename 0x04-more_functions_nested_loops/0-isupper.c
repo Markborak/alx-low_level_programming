723c9eb4f1c243f1d703d68c55bcb213c26a6fb6
Markborak <markborak4@gmail.com>
2023-06-23 06:16:53 -0700

    functions and nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _isupper - Checks uppercase
- *
+ *@c: Parameter
  * Return: Always 1 (success)
  */
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 return (1);
-else 
+else
 return (0);
 }
