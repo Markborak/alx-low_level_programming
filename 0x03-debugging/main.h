d9e169346aeec8d6c90cc467773ee3862bbd5d31
Markborak <markborak4@gmail.com>
2023-06-23 00:41:23 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,6 +8,8 @@
  * @c: third integer
  * Return: the largest number
  */
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 int largest_number(int a,int b,int c);
 
 #endif /* MAIN_H */
