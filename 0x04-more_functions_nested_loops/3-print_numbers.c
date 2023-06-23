ef85a8b55dd105d8b480a9d9db1125a9ff8d00b0
Markborak <markborak4@gmail.com>
2023-06-23 08:02:25 -0700

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * @i: Parameter
+ *
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+
+_putchar(i);
+
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef ISUPPER_H
 #define ISUPPER_H
+int _putchar(char c);
 void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
