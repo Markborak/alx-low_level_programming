fd788eb25be968f659bcbc3bb00de7df34f62e6b
Markborak <markborak4@gmail.com>
2023-06-26 15:37:05 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string, starting with the first character,
+ * puts2 - prints character of a string, starting with the first character,
  *
  * @str: Parameter
  */
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i += 2)
 {
-if (str[i] != '1' && str[i] != '3' && str[i] != '5' && str[i] != '7' && str[i] != '9')
 {
-
 _putchar(str[i]);
 }
 }
