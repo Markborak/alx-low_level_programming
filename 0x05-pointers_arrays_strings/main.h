2d7b63a838ed891c9878401ad09c1f40e584fb6c
Markborak <markborak4@gmail.com>
2023-06-26 15:04:25 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,15 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str);
 void rev_string(char *s);
 int _strlen(char *s);
 void print_rev(char *s);int _strlen(char *s);
