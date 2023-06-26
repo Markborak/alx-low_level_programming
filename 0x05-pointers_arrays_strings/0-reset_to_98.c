da2c2523c18d14f3bc9f4b61efc0436a6d95d5db
Markborak <markborak4@gmail.com>
2023-06-26 01:25:52 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Function that updates the value it points to
+ *
+ * @n: Parameter
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 void reset_to_98(int *n);
 
