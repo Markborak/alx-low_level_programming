29b63fc77080c5dfff6e497af1a2631e37fa8f69
Markborak <markborak4@gmail.com>
2023-07-03 11:37:43 +0300

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
+char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(int c);
 char *_memset(char *s, char b, unsigned int n);
