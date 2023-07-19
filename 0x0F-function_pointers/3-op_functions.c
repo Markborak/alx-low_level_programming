712245043c21a1ed556499b3353329941a442efb
Markborak <markborak4@gmail.com>
2023-07-19 14:14:03 +0300

    Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * op_add - adds two integers
+ *
+ * @a: Parameter
+ *
+ * @b: Parameter
+ *
+ * Return: Always a + b (success)
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - subtracts two integers
+ *
+ * @a; Parameter
+ *
+ * @b: Parmeter
+ *
+ * Return: Always (a-b) (success)
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ *
+ * @a: Parameter
+ *
+ * @b: Parameter
+ *
+ * Return: Always a * b (success)
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - divides two integers
+ *
+ * @a: Parmeter
+ *
+ * @b: Parmeter
+ *
+ * Return: always a / b (success)
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - get modulus
+ *
+ * @a: Parameter
+ *
+ * @b: Parameter
+ *
+ * Return: always a % b success
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
