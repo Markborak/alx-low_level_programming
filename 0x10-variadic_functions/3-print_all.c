333ca643d1eb0477f430039a59ca00d6b86a01d4
Markborak <markborak4@gmail.com>
2023-07-20 12:23:57 +0300

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,48 +10,51 @@
  *
  * @...: variable arguments
  */
+
 void print_all(const char * const format, ...)
 {
 va_list arg;
-
 char ch;
 int i;
 float f;
 char *s;
-
 int index = 0;
+int not_first = 0;
 
 va_start(arg, format);
 
 while (format[index] != '\0')
 {
-if (format[index] == 'c')
+if (not_first)
+printf(", ");
+switch (format[index])
 {
+case 'c':
 ch = va_arg(arg, int);
 printf("%c", ch);
-printf(", ");
-}
-else if (format[index] == 'i')
-{
+not_first = 1;
+break;
+case 'i':
 i = va_arg(arg, int);
 printf("%d", i);
-printf(", ");
-}
-else if (format[index] == 'f')
-{
+not_first = 1;
+break;
+case 'f':
 f = va_arg(arg, double);
 printf("%f", f);
-printf(", ");
-}
-else if (format[index]  == 's')
-{
+not_first = 1;
+break;
+case 's':
 s = va_arg(arg, char*);
 if (s == NULL)
 printf("(nil)");
 else
-{
 printf("%s", s);
-}
+not_first = 1;
+break;
+default:
+not_first = 0;
+break;
 }
 index++;
 }
