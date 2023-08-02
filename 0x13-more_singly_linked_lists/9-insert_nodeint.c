1986530ca8946c1c430c8c168c0720a216e5a2a3
Markborak <markborak4@gmail.com>
2023-08-02 10:09:57 +0300

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_nodeint_at_index - function inserts a new node at a given position
+ * in a listint_t linked list.
+ *
+ * @head: pointer to the pointer of the head of listint_t list
+ *
+ * @idx: index of the list where the new node should be added (starting from 0)
+ *
+ * @n: value to be added to the new node
+ *
+ * Return: The address of the new node (listint_t*), or NULL if it failed.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *current = *head;
+listint_t *new_node = malloc(sizeof(listint_t));
+unsigned int position = 0;
+if (idx == 0)
+{
+return (add_nodeint(head, n));
+}
+/*listint_t *current = *head;*/
+/*unsigned int position = 0;*/
+while (current != NULL && position < idx - 1)
+{
+current = current->next;
+position++;
+}
+if (current == NULL || position < idx - 1)
+{
+return (NULL);
+}
+/*listint_t *new_node = malloc(sizeof(listint_t));*/
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+new_node->next = current->next;
+current->next = new_node;
+return (new_node);
+}
