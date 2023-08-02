025c2692997236f96903e37ceebad94af4707b6d
Markborak <markborak4@gmail.com>
2023-08-02 10:33:07 +0300

    more singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,3 +43,27 @@ new_node->next = current->next;
 current->next = new_node;
 return (new_node);
 }
+
+
+/**
+ * add_nodeint - adds a new node at the beginning of listint_t list
+ *
+ * @head: double pointer to the head of listint_t list
+ *
+ * @n: value to be added to the new node
+ *
+ * Return: Always new node at the beginning (success)
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
